68ac405f8def73d005b70612e01e466e0f4ba08a
kamayoti <kostya.karaman44@gmail.com>
2024-12-19 11:16:34 +0100

    f

--- README.md
+++
@@ -1,2 +0,0 @@
-"portfolio"
--- creater  Karaman Konstantin aka KamaYoti
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./front-end/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
